70f8d53c118a088591e8d90d943e80fc9d0b35a8
Novia Irawati <noviairawati@Novias-MacBook-Air.local>
2024-03-07 20:27:36 +0700

    milestone1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/NtxSJSoQ)
+Hello,
+I' m Ira. For My Assignment I make Online Shop theme : Koper Travel Bag
---
+++ script.js
@@ -0,0 +1,35 @@
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+};
+
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+/*Dark/Light Mode - local storage*/
+const body = document.querySelector("body"),
+  toggle = document.querySelector(".toggle");
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+  body.classList.add("dark");
+  toggle.classList.add("active");
+}
+toggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (!body.classList.contains("dark")) {
+    return localStorage.setItem("mode", "light");
+  }
+  localStorage.setItem("mode", "dark");
+});
+toggle.addEventListener("click", () => toggle.classList.toggle("active"));
